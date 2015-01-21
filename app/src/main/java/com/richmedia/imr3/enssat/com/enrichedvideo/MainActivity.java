446dedfd0e397d1998b7f0d6aefbd89d23b205b5
Marc Poppleton <marc.poppleton@orange.com>
2015-01-21 23:00:40 +0100

    Initialisation du TP

---
+++ app/src/main/java/com/richmedia/imr3/enssat/com/enrichedvideo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.richmedia.imr3.enssat.com.enrichedvideo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initVideo();
+    }
+
+    private void initVideo(){
+        //TODO Récupérer une référence sur la vue ajoutée dans le layout activity_main
+        //TODO Associer un MediaController à la vue ajoutée
+        //TODO Démarrer la lecture de la vidéo
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
